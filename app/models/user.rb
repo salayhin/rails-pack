5d5c1fa2dabbcc555ffd6a6b54734f2950c6215e
Sirajus Salayhin <salayhin@gmail.com>
2015-02-14 18:08:38 +0600

    - Active admin configuired - Cancan gem added - Rolify gem added

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ActiveRecord::Base
+  rolify
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  def is_admin?
+    self.email && ADMIN_EMAIL.to_s.include?(self.email)
+  end
+end
--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,7 @@ module RailsPack
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    ADMIN_EMAIL = 'salayhin@gmail.com'
   end
 end
